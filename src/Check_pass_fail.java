0ace94bdf1107f353576899e84f0900d11d338d0
Saerix <stasmorgun1@gmail.com>
2016-06-29 17:42:16 +0300

    123

--- src/Check_pass_fail.java
+++ src/Check_pass_fail.java
@@ -1,6 +1,6 @@
 //Exercise CheckPassFail (if-else): Write a program called CheckPassFail which prints "PASS"
 // if the int variable "number" is more than or equal to 50;or prints "FAIL" otherwise.
-// The program shall always print “DONE” before exiting.
+// The program shall always print “DONE” before exiting
 
 public class Check_pass_fail {
     static private int number=Count.count();
